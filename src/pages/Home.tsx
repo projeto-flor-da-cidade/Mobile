b43500439b0835c229192c86a25acb86768db173
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2024-12-24 18:20:44 -0300

    mudança na pagina inicial: Logo melhor, box em volta de alguns elementos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar  , IonButton } from '@ionic/react';
+import { IonContent, IonPage, IonButton, } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 import './Home.css';
-import logoPrefeitura from '../assets/logo-prefeitura.png'; 
-import logoProjeto from '../assets/logo-projeto.png';  
-import bannerProjeto from '../assets/banner-projeto.jpg'
+import logoPrefeitura from '../assets/logo-prefeitura.png';
+import logoProjeto from '../assets/logo-projeto.png';
+import bannerProjeto from '../assets/banner-pricipal.svg';
 
 const Home: React.FC = () => {
   const history = useHistory();
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding  centralizar-conteudo" 
-       style={{ '--background': '#316cae'}}>
-      <div className="header-logos">
+    <IonPage className="BakgroundPagina">
+      <IonContent className="ion-padding centralizar-conteudo BakgroundPagina">
+        <div className="header-logos">
           <img src={logoPrefeitura} alt="Logo Prefeitura" className="logo-prefeitura" />
           <img src={logoProjeto} alt="Logo Projeto" className="logo-projeto" />
         </div>
-        <img src={bannerProjeto} alt="Banner Projeto" className="banner-projeto" />
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <div className="container">  <p className="titulo">Secretaria Executiva de Agricultura Urbana 
-          SEAU
-         </p>
-         <IonButton expand="block"  className="botao-personalizado" >
+        <div className='borda-arredondada-banner'>
+          <img src={bannerProjeto} alt="Banner Projeto" className="banner-projeto" />
+        </div>
+        <div className="borda-arredondada">
+          <p className="titulo">
+            Secretaria Executiva de Agricultura Urbana SEAU
+          </p>
+          <IonButton className="botao-personalizado">
             Sou membro da secretaria
           </IonButton>
-          <IonButton expand="block"  className="botao-personalizado"  onClick={() => history.push('/serviços')}>
+          <IonButton
+            className="botao-personalizado"
+            onClick={() => history.push('/serviços')}
+          >
             Não sou membro da secretaria
           </IonButton>
-         </div>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
